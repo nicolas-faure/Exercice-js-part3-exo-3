a52a94d5f1a01426b5d73618978ec3ae6fbe7e49
faure <nicolas.faure201@gmail.com>
2019-01-25 13:43:17 +0100

    exo 3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function green(){
+  document.getElementById('text').style.color ='green'; //change couleur en vert
+}
+function red(){
+  document.getElementById('text').style.color ='red'; //change couleur en rouge
+}
+function blue(){
+  document.getElementById('text').style.color ='blue'; //change couleur en bleu
+}
